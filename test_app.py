8f113023579872bdb756d048772d7fb487b4af23
camarahacks <gustavo-camara@outlook.com>
2025-05-08 22:08:23 -0300

    my first pr

---
+++ test_app.py
@@ -0,0 +1,13 @@
+from fastapi.testclient import TestClient
+from http import HTTPStatus
+
+from fastapi_zero.app import app
+
+
+def test_root_deve_retornar_ola_mundo():
+    client = TestClient(app) #Arrange
+
+    response = client.get('/') #Act
+
+    assert response.status_code == HTTPStatus.OK #Assert
+    assert response.json() == {'message': 'Ola mundo!'} #Assert
