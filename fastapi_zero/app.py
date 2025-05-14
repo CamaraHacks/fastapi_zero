76f26c48e2b356b13509fbb129eb18730bc18f2b
camarahacks <gustavo-camara@outlook.com>
2025-05-14 12:48:36 -0300

    added index path

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,28 @@
 from fastapi import FastAPI
-
+from http import HTTPStatus
+from fastapi.responses import HTMLResponse
 app = FastAPI()
 
+from fastapi_zero.schemas import Message
 
-@app.get('/')
+@app.get('/', 
+         status_code=HTTPStatus.OK,
+         response_model=Message
+         )
 def read_root():
     return {'message': 'Ola mundo!'}
+
+@app.get('/index',
+         response_class=HTMLResponse
+         )
+
+def index():
+    return """
+    <html>
+      <head>
+        <title>Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
