cbf824272375b992392aef15fb647462b0917565
camarahacks <gustavo-camara@outlook.com>
2025-05-14 12:54:02 -0300

    index page test

--- test_app.py
+++ test_app.py
@@ -11,3 +11,14 @@ def test_root_deve_retornar_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK #Assert
     assert response.json() == {'message': 'Ola mundo!'} #Assert
+
+
+def teste_index():
+    client = TestClient(app) #Arrange
+
+    response = client.get('/index') #act
+
+    assert response.status_code == HTTPStatus.OK #Assert
+    assert <h1>Olá mundo </h1> in response.text
+
+
