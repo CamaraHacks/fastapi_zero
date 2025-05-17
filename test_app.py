0175d587c1893b2474f0711ebc3d97305c9cd893
camarahacks <gustavo-camara@outlook.com>
2025-05-16 23:25:52 -0300

    test html added

--- test_app.py
+++ test_app.py
@@ -1,19 +1,29 @@
-from fastapi.testclient import TestClient
 from http import HTTPStatus
+
+from fastapi.testclient import TestClient
 
 from fastapi_zero.app import app
 
 
 def test_root_deve_retornar_ola_mundo():
-    client = TestClient(app) #Arrange
+    client = TestClient(app)  # Arrange
 
-    response = client.get('/') #Act
+    response = client.get('/')  # Act
 
-    assert response.status_code == HTTPStatus.OK #Assert
-    assert response.json() == {'message': 'Ola mundo!'} #Assert
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert response.json() == {'message': 'Ola mundo!'}  # Assert
 
 
-def teste_index():
+def test_html():
+    client = TestClient(app)  # arrange
+
+    response = client.get('/index')
+
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert '<h1> Olá Mundo </h1>' in response.text  # Assert
+
+
+"""def teste_index():
     client = TestClient(app) #Arrange
 
     response = client.get('/index') #act
@@ -21,4 +31,4 @@ def teste_index():
     assert response.status_code == HTTPStatus.OK #Assert
     assert <h1>Olá mundo </h1> in response.text
 
-
+"""
